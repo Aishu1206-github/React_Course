1a0cb1f6b00853dac8e495221d04310eb5ba7d2a
Aishu1206-github <aj5545007@gmail.com>
2025-04-11 14:51:55 +0530

    components are created

---
+++ test/vite-project/src/components/Hello.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <h1>Hello From Hello component...</h1>
+  )
+}
+
+export default Hello
---
+++ test/vite-project/src/components/Hello2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hello2() {
+  return (
+    <h1>Bye From Hello2 component...</h1>
+  )
+}
+
+export default Hello2

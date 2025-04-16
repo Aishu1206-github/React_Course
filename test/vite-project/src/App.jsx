2c0a3bad1dc7fb7c7e39ede1abd85b6ad898bc6c
Aishu1206-github <aj5545007@gmail.com>
2025-04-16 18:55:51 +0530

    Form.jsx file added

--- test/vite-project/src/App.jsx
+++ test/vite-project/src/App.jsx
@@ -5,11 +5,13 @@ import Fruits from './components/Fruits'
 import Hello from './components/hello'
 import Hello2 from './components/Hello2'
 import Counter from './components/Counter'
+import Form from './components/Form'
+
 
 function App() {
   return (
     <>
-    <Counter/>
+    <Form/>
     </>
   )
 }

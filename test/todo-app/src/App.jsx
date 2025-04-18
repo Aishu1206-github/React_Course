011c6a5c6e5430c09b8e541a74f9cd5bfc9baddb
Aishu1206-github <aj5545007@gmail.com>
2025-04-19 01:22:13 +0530

    todo list app added

---
+++ test/todo-app/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <h1> To Do List </h1>
+
+      <div className="input-container">
+        <input type="text"/>
+        <button> + </button>
+      </div>
+
+      <div className='container'>
+        <div className='todo'>
+          <p> go to college...</p>
+          <div className='actions'>
+              <input type='checkbox'/>
+              <button>Delete</button>
+          </div>
+        </div>
+      </div>
+      
+    </main>
+  )
+}
+
+export default App

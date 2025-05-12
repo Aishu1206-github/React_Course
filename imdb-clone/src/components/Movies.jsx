30f724ae61f74e0da3bbd5761e911ffce000f9ec
Aishu1206-github <aj5545007@gmail.com>
2025-05-12 16:03:53 +0530

    Two Compponents are created

---
+++ imdb-clone/src/components/Movies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Movies() {
+  return (
+    <div>
+      <h1> This is a Movies Component</h1>
+    </div>
+  )
+}
+
+export default Movies
--- imdb-clone/src/components/Navbar.jsx
+++ imdb-clone/src/components/Navbar.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import Logo from '../movie.jpg'
+import React from "react";
+import Logo from "../movie.jpg";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
-    <div className='flex border space-x-8 items-center px-3 py-3 font-serif'>
-      <img className='w-[100px] hover:scale-110 transition-transform duration-300' src={Logo} alt="" />
-          <h1 className='text-2xl font-bold tracking-wide '> MoviewFlix</h1>
+    <div className="flex border space-x-8 items-center px-3 py-3 font-serif">
+      <img
+        className="w-[100px] hover:scale-110 transition-transform duration-300"
+        src={Logo}
+        alt=""
+      />
+      <h1 className="text-2xl font-bold tracking-wide "> MoviewFlix</h1>
 
-      <div className='flex space-x-7 text-lg'>
-      <a href="/" className='text-purple-700 transition-colors duration-300 text-xl font-bold'>Home</a>
-      <a href="/watchlist" className='text-purple-700 transition-colors duration-300 text-xl font-bold'>Watchlist</a>
+      <div className="flex space-x-7 text-lg">
+        <Link
+          to="/"
+          className="text-purple-700 transition-colors duration-300 text-xl font-bold"
+        >
+          Movies
+        </Link>
+        <Link
+          to="/watchlist"
+          className="text-purple-700 transition-colors duration-300 text-xl font-bold"
+        >
+          Watchlist
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
